f8bc050f6fceb3bbde3fcee78abb0a0616ad8afa
jose luis <cortezluisjose@gmail.com>
2020-10-19 05:09:06 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,7 +38,16 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-    ##your code here
+    @user = User.find_by(username: params[:username])
+     if @user && @user.authenticate(params[:password])
+       session[:user_id] = @user.id
+
+       redirect to "/account"
+     else
+
+
+       redirect to "/failure"
+     end##your code here
   end
 
   get "/failure" do
